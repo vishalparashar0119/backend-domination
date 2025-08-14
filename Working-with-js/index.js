785b7e5e7656a8a2fc84dbdc53a9a3beb618ac79
vishalparashar0119 <vishalparashar0119@users.noreply.github.com>
2025-08-14 09:39:21 +0530

    array methods Map , Filter , Find , indexOf

--- Working-with-js/index.js
+++ Working-with-js/index.js
@@ -1,1 +1,23 @@
-alert("the js is connected")
+// methods for arrya  map , filter , find , indexOf
+// 1. map 
+
+let array = [1, 2, 3, 4, 5, 6];
+console.log("input Array ::", array);
+
+let newMap = array.map((items) => {
+      return items + 1;
+});
+console.log("map output ::", newMap);
+
+// 2. filter
+let newFilter = array.filter((items) => {
+      return items > 2;
+});
+console.log("Filter Array ::", newFilter);
+
+// 3. find 
+ 
+let newFind = array.find((items) => {
+      return items === 3;
+});
+console.log("Find Array ::", newFind);
