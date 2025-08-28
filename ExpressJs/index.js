9293f6c4d596673ccce302758e7a0d88742dbc83
vishalparashar0119 <vishalparashar0119@users.noreply.github.com>
2025-08-28 15:54:27 +0530

    middle ware is completed

--- ExpressJs/index.js
+++ ExpressJs/index.js
@@ -3,6 +3,16 @@ import express from "express";
 const app =  express();
 const port = 3000;
 
+
+// middle ware in express this is how we write a middle ware
+
+app.use((req , res , next)=>{
+      // this will always run when a request is made 
+      console.log("middle ware chla");
+      // without next the requelst will not pass to request handler or to next middle ware
+      next();
+});
+
 app.get("/" , (req , res) =>{
       // this is also a middle ware
       res.send("this is vishal");
