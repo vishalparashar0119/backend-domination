517122941544bd6a10a0bcfcfdcd15181b2b0506
vishalparashar0119 <vishalparashar0119@users.noreply.github.com>
2025-09-02 15:10:38 +0530

    ejs with project

---
+++ EJS/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log();
+// parser for form data 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// setting up view engine 
+app.set('view engine' , 'ejs');
+
+// setting up  route 
+app.get('/' , (req , res) =>{
+      res.render("index");
+});
+
+// listen server 
+app.listen(port ,()=>{
+      console.log(`server is running on http://localhost:${port}`);
+});
